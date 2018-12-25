314bb25d4f274fde6fecabeefcec4f0125ce8436
blusky <git@blusky.fr>
2018-12-25 09:53:58 +0100

    Boiler plating tests

--- run_tests.bat
+++ run_tests.bat
@@ -1,5 +1,8 @@
 @echo off
-docker stop mongo
-docker run --rm -d --name mongo mongo
+echo + Preparing env
+docker stop mongo 2>NUL
+docker run --rm -d --name mongo mongo > NUL
+echo + Starting tests
 docker run --rm -v %cd%:/opt/tromino:ro -w /opt/tromino/src --link mongo theblusky/tromino:dev sh run_tests.sh
-docker stop mongo
+echo + Cleaning env
+docker stop mongo 2>NUL
--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-docker stop mongo
-docker run --rm -d --name mongo mongo
+echo "+ Preparing env"
+docker stop mongo 2>/dev/null
+docker run --rm -d --name mongo mongo > /dev/null
+echo "+ Starting tests"
 docker run --rm -v $(pwd):/opt/tromino:ro -w /opt/tromino/src --link mongo theblusky/tromino:dev sh run_tests.sh
+echo "+ Cleaning env"
 docker stop mongo 2>/dev/null
