985a27019a6752d86e141a8575b1de12eabf0127
blusky <git@blusky.fr>
2018-12-25 23:36:33 +0100

    Models testing boilerplate

--- src/tests/models.py
+++ src/tests/models.py
@@ -7,25 +7,16 @@ from models.parameter import ParameterModel
 
 
 def async_test(f):
-    def wrapper(*args, **kwargs):
-        if inspect.iscoroutinefunction(f):
-            future = f(*args, **kwargs)
-        else:
-            coroutine = asyncio.coroutine(f)
-            future = coroutine(*args, **kwargs)
-        asyncio.get_event_loop().run_until_complete(future)
-
-    return wrapper
+    return lambda *args, **kwargs: asyncio.new_event_loop().run_until_complete(
+        f(*args, **kwargs)
+        if inspect.iscoroutinefunction(f)
+        else asyncio.coroutine(f)(*args, **kwargs)
+    )
 
 
 class ModelsTestCase(unittest.TestCase):
-    loop = None
-
     def setUp(self):
         logging.basicConfig(level=logging.ERROR)
-        if not ModelsTestCase.loop:
-            ModelsTestCase.loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(ModelsTestCase.loop)
 
     @async_test
     async def test_00_flush(self):
