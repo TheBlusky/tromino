0de40490c52eb17f042f5d6d2d67319579ea2dbd
blusky <git@blusky.fr>
2019-01-02 21:13:58 +0100

    Monitors tests boilerplate

--- src/tests/__init__.py
+++ src/tests/__init__.py
@@ -1,2 +1,3 @@
 from tests.mattermost import MattermostTestCase
 from tests.models import ModelsTestCase
+from tests.monitors import MonitorsTestCase
---
+++ src/tests/monitors.py
@@ -0,0 +1,30 @@
+import asyncio
+import inspect
+import logging
+import unittest
+
+from exceptions import MonitorAlreadyExists, NoSuchMonitor, ParameterAlreadyExists
+from models.monitor import MonitorModel
+from models.parameter import ParameterModel
+
+
+def async_test(f):
+    return lambda *args, **kwargs: asyncio.new_event_loop().run_until_complete(
+        f(*args, **kwargs)
+        if inspect.iscoroutinefunction(f)
+        else asyncio.coroutine(f)(*args, **kwargs)
+    )
+
+
+class MonitorsTestCase(unittest.TestCase):
+    def setUp(self):
+        logging.basicConfig(level=logging.ERROR)
+
+    @async_test
+    async def test_00_flush(self):
+        await ParameterModel.flush()
+        await MonitorModel.flush()
+
+    @async_test
+    async def test_01_init(self):
+        pass
